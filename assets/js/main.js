964ad83ecc1220f39e02fab5fa3b4f750888f3f7
bezumkin <bezumkin@yandex.ru>
2015-08-23 16:32:17 +0700

    Работа с history.js в ajax-навигации

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,41 @@
+var oldBrowser = !(window.history && history.pushState);
+if (!oldBrowser) {
+	history.replaceState({pagination: window.location.href}, '');
+}
+
+function loadPage(href) {
+	var wrapper = $('#news-wrapper');
+	wrapper.css('opacity', .5);
+	$.get(href, function(res) {
+		wrapper.css('opacity', 1);
+		if (res.success) {
+			$('#news-items').html(res.data['items']);
+			$('#news-pagination').html(res.data['pagination']);
+		}
+		else if (res.data['redirect']) {
+			window.location = res.data['redirect'];
+		}
+		else {
+			console.log(res);
+		}
+	}, 'json');
+}
+
 $('#news-wrapper').on('click', '#news-pagination a', function() {
 	var href = $(this).attr('href');
 
 	if (href != '') {
-		var wrapper = $('#news-wrapper');
-		wrapper.css('opacity', .5);
-		$.get(href, function(res) {
-			wrapper.css('opacity', 1);
-			if (res.success) {
-				$('#news-items').html(res.data['items']);
-				$('#news-pagination').html(res.data['pagination']);
-			}
-			else if (res.data['redirect']) {
-				window.location = res.data['redirect'];
-			}
-			else {
-				console.log(res);
-			}
-		}, 'json');
+		if (!oldBrowser) {
+			window.history.pushState({pagination: href}, '', href);
+		}
+		loadPage(href);
 	}
 
 	return false;
+});
+
+$(window).on('popstate', function(e) {
+	if (e.originalEvent.state && e.originalEvent.state['pagination']) {
+		loadPage(e.originalEvent.state['pagination']);
+	}
 });
